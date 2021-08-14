bff8d12a5cfce4e44fcd4d8cee983f69aee8ba4f
Ashutosh Vishnoi <ashutoshvishnoi75@gmail.com>
2021-08-14 16:28:50 +0530

    first commit

---
+++ src/components/CharacterInList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Character = ({character}) => {
+	
+	const {name, occupation, birthday, status} = character;
+	
+  return (
+    <div>
+    
+    </div>
+  );
+};
+
+export default Character;
